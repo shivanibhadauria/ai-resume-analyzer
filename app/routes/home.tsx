bea5dab8a2c17b8b42f233e5ab1886b567e7e581
shivanibhadauria <shivanibhadauria921997@gmail.com>
2025-08-31 20:03:08 +0530

    feat(auth): add authentication check and Zustand state management

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,8 +2,21 @@ import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "constants/data";
 import ResumeCard from "~/components/ResumeCard";
+import React, { useEffect } from "react"
+import { useLocation, useNavigate } from "react-router"
+import { usePuterStore } from "~/lib/puter"
+
 
 export function meta({}: Route.MetaArgs) {
+
+ const { auth} = usePuterStore(); 
+ const navigate = useNavigate();
+
+  useEffect(() => {
+      if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated ])
+
+
   return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job" },
@@ -11,10 +24,10 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover "  >
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover  "  >
     <Navbar />
     <section className="main-section" >
-      <div className="page-heading" >
+      <div className="page-heading py-16" >
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submission and check AI-powered feedback.</h2>
       </div>
