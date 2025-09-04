203ba0fa8e1531652d9323a44593b60a77a447b8
shivanibhadauria <shivanibhadauria921997@gmail.com>
2025-09-04 12:26:07 +0530

    feat(resume-feedback): implement resume score visualization with gauge component

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import ScoreGuage from './ScoreGauge'
 
-const Summary = () => {
+
+const Category = ({title , score}: {title: string, score: number}) => {
   return (
-    <div>Summary</div>
+    <div className='resume-summary'>
+    {title} -{score}
+    </div>
+  )
+}
+const Summary = ({feedback }: {feedback : Feedback}) => {
+  return (
+    <div className='bg-white rounded-2xl shadpw-md w-full' >
+      <div className='flex flex-row items-center p-4 gap-8' >
+        <ScoreGuage score={feedback.overallScore} />
+        <div className='flex flex-col gap-2 ' >
+          <h2 className='text-2xl font-bold' > Your Resume Score</h2>
+          <p className='text-sm text-gray-500'>
+            This score id calculated based on the variable listed below.
+          </p>
+        </div>
+      </div>
+
+<Category title='Tone and Style' score={feedback.toneAndStyle?.score} />
+<Category title='Tone and Style' score={feedback.content?.score} />
+<Category title='Tone and Style' score={feedback.structure?.score} />
+<Category title='Tone and Style' score={feedback.skills?.score} />
+
+    </div>
   )
 }
 
